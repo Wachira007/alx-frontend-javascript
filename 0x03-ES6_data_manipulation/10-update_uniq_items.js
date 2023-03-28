79126826fd783bce8a37caee73ca86bdbf2f1584
Wachira007 <dannielmutua00@gmail.com>
2023-03-28 22:21:00 +0300

    jest

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,14 @@
 export default function updateUniqueItems(map) {
     const list = map;
-  
+    
     if (list instanceof Map) {
-      for (const [key, value] of list) {
-        if (value === 1) {
-          list.set(key, 100);
+        for (const [key, value] of list) {
+            if (value === 1) {
+                list.set(key, 100);
+            }
         }
-      }
     } else {
-      throw new Error('Cannot process');
+        throw new Error('Cannot process');
     }
     return list;
-  }
+}
