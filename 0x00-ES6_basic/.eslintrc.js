caacb2397c605b0d82bb65a2661af6081ce54d7f
Wachira007 <dannielmutua00@gmail.com>
2023-03-28 12:09:39 +0300

    Corrections onthe configuration files

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,15 +1,35 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true
+    browser: false,
+    es6: true,
+    jest: true,
   },
-  extends: 'standard-with-typescript',
-  overrides: [
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
   rules: {
-  }
-}
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
